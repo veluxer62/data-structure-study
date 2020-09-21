4175d853afb0d887f2921d33d59803438ecb6bc5
k <k@101.inc>
2020-09-22 00:15:17 +0900

    init

---
+++ src/main/kotlin/com/example/datastructurestudy/DataStructureStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.datastructurestudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DataStructureStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<DataStructureStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/datastructurestudy/DataStructureStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.datastructurestudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DataStructureStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
